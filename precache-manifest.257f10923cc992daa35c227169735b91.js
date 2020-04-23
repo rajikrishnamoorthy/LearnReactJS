e76b56f6004182f1c7070f68bac80ad1a08b2d9c
perumalraji <perumalraji@hotmail.com>
2020-04-24 04:33:20 +0530

    Updates

---
+++ precache-manifest.257f10923cc992daa35c227169735b91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22644f54592324a82462e62c0e5c15b2",
+    "url": "/LearnReactJS/index.html"
+  },
+  {
+    "revision": "bbb2840dde8da0e3840e",
+    "url": "/LearnReactJS/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "79db7d4981814e0cb005",
+    "url": "/LearnReactJS/static/js/2.455f1b76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LearnReactJS/static/js/2.455f1b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb2840dde8da0e3840e",
+    "url": "/LearnReactJS/static/js/main.09181df1.chunk.js"
+  },
+  {
+    "revision": "f9ce79d9176f6177a735",
+    "url": "/LearnReactJS/static/js/runtime-main.f1843170.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LearnReactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
